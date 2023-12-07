5040c3fec1f5d2516b721fab28c3bedf9e3cb6ff
Jean-Laurent Wotton <jlwotton@gmail.com>
2023-12-07 23:43:14 +0100

    extract 10 pros/cons from files with name starting 'review'

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,14 @@ import { getPdfText } from "./get-pdf";
 import rl from "readline/promises";
 import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
 import { getFiles, saveResult } from "./files";
+import path from "path";
 
 const readline = rl.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const scooter = resolve("./docs/LG-SQC4R.pdf");
+//const scooter = resolve("./docs/LG-SQC4R.pdf"); TODELETE
 
 const openai = new OpenAI({
   apiKey: process.env["OPENAI_KEY"],
@@ -23,13 +24,59 @@ async function main() {
   for (const file of files) {
     console.log("Handling ", file);
 
-    await handleFile(file);
+    if (! path.basename(file).endsWith(".pdf")) { 
+      console.log('Warning: not a PDF file...\n\n');
+     }
+    else {
+      await handleFile(file);
+     }
   }
 }
 
 async function handleFile(file: string) {
   const pdfContent = await getPdfText(file);
   console.log("Got PDF content: ", pdfContent.length);
+
+  let userContent = ""; // depending on the name of the file, the user prompt will be different
+
+  if (path.basename(file).startsWith("review")) {
+    console.log("Review document");
+    userContent = `Parse the following document that contains a list of user reviews for a product. Write 5 positive statements and 5 negative statements on this product. Format it in English in a raw JSON format (not Markdown, just the JSON)\n
+         
+    The content to parse is the following:\n\n
+    `;
+
+  } else {
+    console.log("Product document");
+    userContent = `Please parse the following and format it in English in a raw JSON format (not Markdown, just the JSON) with the following properties:\n
+        
+    {
+      "product_name": "name of the product",
+      "brand": "brand of the product",
+      "description": "description of the product",
+      "features": [
+        "feature 1",
+        "feature 2",
+        "feature 3"
+      ],
+      "compatibility": {
+        "airplay": true, // or false, or null if unknown
+        "bluetooth": true, // or false, or null if unknown
+        "dolby_atmos": true, // or false, or null if unknown
+        "dts_x": true, // or false, or null if unknown
+        "google_cast": true, // or false, or null if unknown
+        "spotify_connect": true, // or false, or null if unknown
+      },
+      "price": {
+          "amount" : 100.00, // or null if unknown
+          "currency": "As 3-letter ISO code, for example USD" // or null if unknown
+      }
+    }
+
+    The content to parse is the following:\n\n
+    `;
+  }
+
   const chat: ChatCompletionMessageParam[] = [
     {
       role: "system",
@@ -40,35 +87,9 @@ async function handleFile(file: string) {
     {
       role: "user",
       content:
-        `Please parse the following and format it in English in a raw JSON format (not Markdown, just the JSON) with the following properties:\n
-        
-        {
-          "product_name": "name of the product",
-          "brand": "brand of the product",
-          "description": "description of the product",
-          "features": [
-            "feature 1",
-            "feature 2",
-            "feature 3"
-          ],
-          "compatibility": {
-            "airplay": true, // or false, or null if unknown
-            "bluetooth": true, // or false, or null if unknown
-            "dolby_atmos": true, // or false, or null if unknown
-            "dts_x": true, // or false, or null if unknown
-            "google_cast": true, // or false, or null if unknown
-            "spotify_connect": true, // or false, or null if unknown
-          },
-          "price": {
-              "amount" : 100.00, // or null if unknown
-              "currency": "As 3-letter ISO code, for example USD" // or null if unknown
-          }
-        }
-
-        The content to parse is the following:\n\n
-        ` + pdfContent,
+        userContent + pdfContent,
     },
-  ];
+    ];
 
   const chatCompletion = await openai.chat.completions.create({
     messages: chat,
